1a2c2e2ee86bb3eaa9088ac04e9f0a436cc61f9d
AlexSanLo <san.lope.alex@gmail.com>
2024-12-02 22:11:43 +0100

    Sexta Version

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./src", // Cambia la raíz del proyecto a "src"
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        about: resolve(__dirname, "src/index2.html")
+      },
+    },
     outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
   },
+  base: "./",
 });
